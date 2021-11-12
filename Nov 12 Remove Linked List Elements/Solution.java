ed3e64391ad838c4cd20a3473940bd3716cfc6a6
Selva Lakshmi S.V <76725996+SelvaLakshmiSV@users.noreply.github.com>
2021-11-12 20:12:04 +0530

    Create Solution.java

---
+++ Nov 12 Remove Linked List Elements/Solution.java
@@ -0,0 +1,30 @@
+class Solution 
+{
+    public ListNode removeElements(ListNode head, int val) 
+    {
+        
+        if(head == null) return null;
+        
+        ListNode current = head;
+        
+        while(head != null && head.val == val)
+        {
+            head = head.next;
+        }
+            
+        while(current != null && current.next != null)
+        {
+            if(current.next.val == val)
+            {
+                current.next = current.next.next;
+            }
+            else
+            {
+                current = current.next;
+            }
+            
+        }
+        return head;
+    }
+}
+
